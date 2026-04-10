d7c8df516fd0ba418717068e7136dedfdedb1a22
José Andrés sedán Espitia <109873223+jasedane75@users.noreply.github.com>
2026-04-10 17:44:58 -0500

    Create customers_landing.sql

---
+++ landinsg_sql/customers_landing.sql
@@ -0,0 +1,23 @@
+-- Landing customers
+CREATE EXTERNAL TABLE IF NOT EXISTS `db_test`.`raw_ customers` (
+  `serialnumber` string,
+  `sharewithpublicasofdate` string,
+  `birthday` string,
+  `registrationdate` string,
+  `sharewithresearchasofdate` string,
+  `customername` string,
+  `email` string,
+  `lastupdatedate` string,
+  `lastupdatedate` string,
+  `sharewithfriendsasofdate` string
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+WITH SERDEPROPERTIES (
+  'ignore.malformed.json' = 'FALSE',
+  'dots.in.keys' = 'FALSE',
+  'case.insensitive' = 'TRUE',
+  'mapping' = 'TRUE'
+)
+STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION 's3://jsedan-files/customer/landing/'
+TBLPROPERTIES ('classification' = 'json');
